7b2590c17d27c71a7d36c62c5df6a3b5ef281e80
Your Name <fantastizeey@gmail.com>
2024-06-04 20:52:40 +0100

    sec commit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 
-import PromptCard from "./PromptCard";
+import PromptCard from "./Promptcard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
